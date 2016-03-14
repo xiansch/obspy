bc79dc1f075b19f4da11460889758f937090752e
Tobias Megies <megies@geophysik.uni-muenchen.de>
2016-03-14 14:04:15 +0100

    refactor new rotation of streams to ZNE based on channel metadata
    ..(e.g. components "Z", "1" and "2" of borehole instruments) to use common
    Stream.rotate() interface.

--- obspy/core/tests/test_stream.py
+++ obspy/core/tests/test_stream.py
@@ -2514,7 +2514,7 @@ class StreamTestCase(unittest.TestCase):
              "2016-03-11T11:34:44.025000Z", 'station': 'FFB3'}]
         for metadata in (inv, parser):
             st = read("/path/to/ffbx_unrotated_gaps.mseed", format="MSEED")
-            st.rotate_to_zne(metadata)
+            st.rotate("->ZNE", metadata=metadata)
             # do some checks on results
             self.assertEqual(len(st), 30)
             # compare data
